fb666b901bf83894b5c1e2823d578bdfaab2e7bb
raidbos <den1998.bro@gmail.com>
2017-05-12 14:44:24 +0300

    version1

---
+++ app/views/account/navigation_bottom.blade.php
@@ -0,0 +1,6 @@
+<div class="footer">
+	<div class="container">
+		<b class="copyright">&copy; 2017 Automated Library Management System </b> Designed by Denys Piskovoy. All rights reserved.
+		<br><a href="https://github.com/RaidBos/Automated-Library-Management-System" target="_blank">Fork on Github</a>
+	</div>
+</div>
